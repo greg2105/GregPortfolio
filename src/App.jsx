d83fc00859af1fcb41289c0753c2b772cb8fe082
greg2105 <kinggreg2105@gmail.com>
2024-11-23 17:41:25 -0500

    added contact icons, webm vid, backdrop. Next step is to fix the glass-pane class and element positioning.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import github from './assets/github.svg';
+import mail from './assets/mail.svg';
+import scroll from './assets/scrollDownTransparent.gif';
+import programmer from './../public/programmer.webm';
+import linkedIn from './assets/linkedIn.svg';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div class='firstDiv'>
-      <h1>Hello this is my portfolio</h1>
-      <h2>this is another header, but smaller</h2>
+      <div class='Introduction'>
+        <div class='Header'>
+          <div class='icons'>
+            <img class='github' src={github} alt="github" />
+            <img class='mail' src={mail} alt="mail" />
+            <img class='linkedIn' src={linkedIn} alt="linkedIn" />
+          </div>
+        </div>
+        <div class='Title'>
+          <h1 class='Text'>Hello, my name is Greg Dorazio</h1>
+        </div>
+        <div class='programmer-container'>
+          <video class='programmer' autoPlay loop muted>
+            <source src={programmer} type="video/webm"/>
+            Your browser can't display this really cool webm video
+          </video>
+        <div class='line-and-text'>
+          <div class='glass-pane'>
+            <p class='text'> I am a new graduate from Penn State with a degree in Computer Science. I am based in Yorktown Heights, New York  </p>
+          </div>
+          <div class='line'></div>
+        </div>
+      </div>
+        {/* <div class='Body'>
+          <h2>Webdev, ML</h2>
+          <h3>Yorktown Heights, NY</h3>
+        </div> */}
+      </div>
+      <div class='Footer'>
+          <img class='scroll' src={scroll} alt="scroll"/>
+      </div>
+      {/* <div class='Education'>
+        <h1>penn state</h1>
       </div> 
-      <div class='secondDiv'>
-      <h1>Hello this is my portfolio</h1>
-      <h2>this is another header, but smaller</h2>
-      </div> 
-      <div class='thirdDiv'>
-      <h1>Hello this is my portfolio</h1>
-      <h2>this is another header, but smaller</h2>
-      </div> 
-      <div class='fourthDiv'>
-      <h1>Hello this is my portfolio</h1>
-      <h2>this is another header, but smaller</h2>
-      </div> 
+      <div class='Projects'>
+        <h1>inside the lumen</h1>
+      </div> */}
     </>
   )
 }
